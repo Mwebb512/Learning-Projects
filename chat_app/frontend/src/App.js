038ceaf6473de205928273e56b8a3067af991d08
matthew webb <120092335+Mwebb512@users.noreply.github.com>
2024-04-03 18:20:24 -0400

    Initial setup for chat web application

---
+++ chat_app/frontend/src/App.js
@@ -0,0 +1,27 @@
+// App.js
+import React, { Component } from "react";
+import "./App.css";
+import { connect, sendMsg } from "./api";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    connect();
+  }
+
+  send() {
+    console.log("hello");
+    sendMsg("hello");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button className="blue-button" onClick={this.send}>Hit</button>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
---
+++ chat_app/frontend/src/api/index.js
@@ -0,0 +1,38 @@
+//api/index.js
+var socket = new WebSocket("ws://localhost:8080/ws");
+
+//interacts with out server that was setup in main.go
+
+//this function connects to the websocket endpoint
+//and listens for events.
+let connect = () =>{
+    console.log("Attempting Connection...")
+
+    socket.onopen = () => {
+        console.log("Successfully Connected");
+    };
+
+    socket.onmessage = msg => {
+        console.log(msg);
+    };
+
+    socket.onclose = event =>{
+        console.log("Socket Closed Connection: ", event);
+    };
+
+    socket.onerror = error => {
+        console.log("Socket Error: ", error);
+    };
+};
+
+//this function allows us to send messages from our frontend
+//to our backend via our WebSocket using socket.send(msg)
+
+let sendMsg = msg => {
+    console.log("sending msg: ", msg);
+    socket.send(msg);
+};
+
+export { connect, sendMsg };
+
+//need to update the app.js to add these functions
